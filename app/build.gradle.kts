a6443dfab35fdc7b825f2b87cd0387cc5152f9e9
500a5 <500a5@mail.ru>
2025-04-22 15:28:53 +0300

    init hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -53,4 +55,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Dagger Hilt
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.android)
+
+    // Hilt Jetpack components
+     //implementation(libs.hilt.navigation.compose)
+    //implementation(libs.androidx.hilt.lifecycle.viewmodel)
 }
---
+++ app/src/main/java/soft/divan/moodtracker/App.kt
@@ -0,0 +1,8 @@
+package soft.divan.moodtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+}
