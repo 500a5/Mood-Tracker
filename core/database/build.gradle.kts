4e24547378a821bbf717c3546b5afb56e39e197f
500a5 <500a5@mail.ru>
2025-04-28 13:04:15 +0300

    init module core database

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,13 +1,18 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "soft.divan.moodtracker.core.designsystem"
+    namespace = "soft.divan.moodtracker.core.database"
     compileSdk = libs.versions.compileSdk.get().toInt()
 
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
     defaultConfig {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -21,19 +26,12 @@ android {
     kotlinOptions {
         jvmTarget = libs.versions.java.get()
     }
-
-    buildFeatures {
-        compose = true
-    }
-
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
